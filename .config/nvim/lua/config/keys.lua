104eec894aef508d796662e0b8564919328064e3
Daniel Soto <73802037+dxnst@users.noreply.github.com>
2025-10-01 06:28:59 -0600

    preparing for a zettelkasten typst setup

--- .config/nvim/lua/config/keys.lua
+++ .config/nvim/lua/config/keys.lua
@@ -1,1 +1,11 @@
 -- General settings
+
+-- Basic navigation and editing
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
+vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit" })
+
+-- Load custom Typst helpers
+local typst_links = require("config.typst-links")
+vim.keymap.set("n", "<leader>zi", typst_links.insert_note_id, { desc = "Insert note [I]D" })
+vim.keymap.set("n", "<leader>zr", typst_links.insert_related_note, { desc = "Insert [R]elated note entry" })
+
