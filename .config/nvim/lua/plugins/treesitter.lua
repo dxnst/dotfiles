7bbe06c3db819813d543496a5b25ca1af10b3656
Daniel Soto <73802037+dxnst@users.noreply.github.com>
2026-01-11 10:29:03 -0600

    treesitter changes

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,26 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-	lazy = false,
+    lazy = false,
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter.configs").setup({
+        ---@diagnostic disable-next-line: missing-fields
+        require("nvim-treesitter").setup({
             ensure_installed = {
                 "latex", "lua", "make", "python", "rust", "typst"
             },
             sync_install = false,
             auto_install = true,
-            highlight = {
-				enable = true
-			},
-			indent = {
-                enable = true
-            },
         })
+
+        -- Enable treesitter-based highlighting
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "*",
+            callback = function()
+                pcall(vim.treesitter.start)
+            end,
+        })
+
+        -- Enable treesitter-based indentation
+        vim. opt.indentexpr = "v: lua.require'nvim-treesitter'.indentexpr()"
     end
-};
+}
